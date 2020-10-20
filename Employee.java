db2d0d205622d2151ec12e2db4f0c84d3a841189
Avenger2627 <imraninfo97@gmail.com>
2020-10-20 17:18:52 +0530

    Added:This check employee is parttime or full time and calculate wages

--- Employee.java
+++ Employee.java
@@ -1,34 +1,45 @@
 package com.Employee;
 
-public class Employee {
-
+public class Employee 
+{
 	public static void main(String[] args) 
 	{
 		System.out.println("******Welocme to Employee Wages Problem*******");
-//EMPLOYEE IS PRESNT OR NOT USE CASE 1
+		
+		int Is_Present=1;
+		int Salary=0;
+		int Is_Part_Time=0;
 		int Is_Full_Time=1;
 		int EmpRate_Hour=20;
 		int EmpHrs=8;
-		int Salary=0;
+		int Part_Time_Hr=4;
 		
-		
-		
+//Genrating Random Number
 		int EmpCheck = (int) (Math.floor(Math.random() *10 % 2));
-		if (EmpCheck==Is_Full_Time)
+		int EmpCheckT = (int) (Math.floor(Math.random() *10 % 2));
+//Checking Employee And Caluclating Wages
+		if (EmpCheck == Is_Present)
 		{
-			
-			Salary=(EmpHrs*EmpRate_Hour);
-			System.out.println("Employee Is Present and Salary is: " +Salary);
+			if (EmpCheckT == Is_Full_Time)
+			{
+				Salary=EmpHrs*EmpRate_Hour;
+				System.out.println("Employee Is Present and Salary is: " +Salary);
 				
+			}
+							
+			else
+			{
+				Is_Part_Time=EmpRate_Hour*Part_Time_Hr;
+				System.out.println("Employee Is Present and Salary is: " +Is_Part_Time);
+					
+			} //Else Block Over
+		
 		}
 		else
 		{
-			System.out.println("Employee Is Not Present and Salary is :" +Salary);
-		
-		} //Else Block Over
-		
+			System.out.println("Employee Is Absent & Salary is :" +Salary);
+		}
 
-		
-	}
+	} //Public
 
-}
+}// MAin Class
