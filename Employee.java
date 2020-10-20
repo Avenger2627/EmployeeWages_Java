b0223cb38b70c04e381d848307259ab9e33779cb
Avenger2627 <imraninfo97@gmail.com>
2020-10-20 15:56:47 +0530

    Added:Check Employee is Present or not

--- Employee.java
+++ Employee.java
@@ -1,12 +1,27 @@
 package com.Employee;
 
-public class Employee 
-{
+import java.util.Scanner;
+
+public class Employee {
 
 	public static void main(String[] args) 
 	{
-		System.out.println("********Welcome To Employee Wages********");
+		System.out.println("******Welocme to Employee Wages Problem*******");
+//EMPLOYEE IS PRESNT OR NOT USE CASE 1
+		int Is_Full_Time=1;
+		int EmpCheck = (int) (Math.floor(Math.random() *10 % 2));
+		System.out.println(EmpCheck);
+		if (EmpCheck==Is_Full_Time)
+		{
+			System.out.println("Employee Is Present");
+		}
+		else
+		{
+			System.out.println("Employee Is Not Present");
+		} //Else Block Over
+		
 
+		
 	}
 
 }
