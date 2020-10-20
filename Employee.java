829a9340509c2ef2586f9b167cd93b80f7f5b020
Avenger2627 <imraninfo97@gmail.com>
2020-10-20 16:20:07 +0530

    Added:Calculate Employee Wages

--- Employee.java
+++ Employee.java
@@ -7,15 +7,24 @@ public class Employee {
 		System.out.println("******Welocme to Employee Wages Problem*******");
 //EMPLOYEE IS PRESNT OR NOT USE CASE 1
 		int Is_Full_Time=1;
+		int EmpRate_Hour=20;
+		int EmpHrs=8;
+		int Salary=0;
+		
+		
+		
 		int EmpCheck = (int) (Math.floor(Math.random() *10 % 2));
-		System.out.println(EmpCheck);
 		if (EmpCheck==Is_Full_Time)
 		{
-			System.out.println("Employee Is Present");
+			
+			Salary=(EmpHrs*EmpRate_Hour);
+			System.out.println("Employee Is Present and Salary is: " +Salary);
+				
 		}
 		else
 		{
-			System.out.println("Employee Is Not Present");
+			System.out.println("Employee Is Not Present and Salary is :" +Salary);
+		
 		} //Else Block Over
 		
 
