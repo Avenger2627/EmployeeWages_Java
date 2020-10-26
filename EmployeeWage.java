a95977e6e47256fdd497e7d20b2ce69d5b2046da
Avenger2627 <68658753+Avenger2627@users.noreply.github.com>
2020-10-26 18:30:56 +0530

    Added UC 14 Final Result

---
+++ EmployeeWage.java
@@ -0,0 +1,58 @@
+package com.bridgelabz;
+
+public class EmpSimple {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+
+	private final int empRatePerHour;
+	private final int numOfWorkingDays;
+	private final int maxhoursPerMonth;
+	private final String company;
+	private int totalEmpWage;
+	public EmpSimple(String company, int empRatePerHour, int numOfWorkingDays, int maxhoursPerMonth)
+	{
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.numOfWorkingDays=numOfWorkingDays;
+		this.maxhoursPerMonth=maxhoursPerMonth;
+	}
+	public void computeEmpWage() {
+		int emphrs = 0, totalworkingdays = 0, totalEmphrs = 0;
+		while (totalEmphrs < maxhoursPerMonth && totalworkingdays < numOfWorkingDays) {
+			totalworkingdays++;
+			int random = (int) Math.floor(Math.random() * 10) % 3;
+			switch (random) {
+
+			case 1:
+				emphrs = 8;
+				break;
+
+			case 2:
+				emphrs = 4;
+				break;
+
+			default:
+				emphrs = 0;// not present
+
+			}
+			totalEmphrs += emphrs;
+			System.out.println("days" +totalworkingdays+ "Emphrs" +emphrs);
+		}
+		totalEmpWage = totalEmphrs * empRatePerHour;
+		
+	}
+	//@override
+	public String toString(){
+	return "Total Emp wages for company " +company+ "is" +totalEmpWage;
+	}
+
+	public static void main(String[] args) {
+		EmpSimple info = new EmpSimple("infosys" ,20,2,10);
+		EmpSimple cogni = new EmpSimple("cognizath" ,40,4,20);
+		info.computeEmpWage();
+		System.out.println(info);
+		cogni.computeEmpWage();
+		System.out.println(cogni);
+		
+	}
+}
